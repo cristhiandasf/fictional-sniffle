bc8be3df39d4b568d60551c51f0d52a84190faa2
Cristhian Yeison Da Silva Fernandez <cristhian.d@thoughtworks.com>
2022-08-13 17:20:09 +0100

    Add generate xml test

--- test_xsl_to_xml.py
+++ test_xsl_to_xml.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch, call
+
 from xsl_to_xml import XslToXml
 
 
@@ -14,3 +16,14 @@ class TestXslToXml:
         actual_response = self.xsl_to_xml_instance.get_file_name("/Users/zfictional-sniffle/SampleXsl.xsl")
         assert actual_response == expected_response
 
+    # Investigate mocks/spies/stubs
+
+    @patch('os.walk')
+    @patch('builtins.open')
+    def test_generate_xml_files(self, mock_open, mock_walk):
+        mock_walk.return_value = [("/Users/fictional-sniffle/", ["dir"], ["SampleXsl.xsl"])]
+
+        self.xsl_to_xml_instance.generate_xml_files()
+
+        mock_open.called_with("/Users/fictional-sniffle/SampleXsl.xsl")
+        mock_open.called_with("SampleXsl_lang.xml", "w")
--- xsl_to_xml.py
+++ xsl_to_xml.py
@@ -30,37 +30,14 @@ class XslToXml:
         file.close()
         return lines
 
-    def copy_last_line(self, lines, xml_file):
-        xml_file.write(lines[-1])
-
-    def copy_first_2_lines(self, lines, xml_file):
-        xml_file.write(lines[0])
-        xml_file.write(lines[1])
-
-    def get_indexes(self, lines):
-        start_index = 0
-        end_index = 0
-        for line in lines:
-            if OPENING_TAG in line:
-                start_index = lines.index(line) + 1
-            if CLOSING_TAG in line:
-                end_index = lines.index(line)
-        return start_index, end_index
-
-    def write_tags(self,end_index, lines, xml_file, star_index):
-        while star_index < end_index:
-            xml_file.write(lines[star_index])
-            star_index = star_index + 1
+    def write_tags(self, lines, xml_file):
+        xml_file.write(lines)
 
     def create_file(self, file_path: str):
         file_name = self.get_file_name(file_path)
         lines = self.get_file_lines(file_path)
-
         with open(f"{file_name}.xml", "w") as xml_file:
-            self.copy_first_2_lines(lines, xml_file)
-            star_index, end_index = self.get_indexes(lines)
-            self.write_tags(end_index, lines, xml_file, star_index)
-            self.copy_last_line(lines, xml_file)
+            self.write_tags(lines, xml_file)
 
     def generate_xml_files(self):
         for directory in self.get_list_of_directories():
